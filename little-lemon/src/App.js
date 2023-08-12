c955f578081767bdd21f3a449c4250e37e22d50d
KarimDev7 <keldeyry@talentschmiede.com>
2023-08-12 14:40:22 +0200

    Logical Setup & HTML+CSS Basics

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-      This is some text
-    </div>
+    <>
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ little-lemon/src/components/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Nav.css";
+import logo from '../assets/Logo.svg';
+
+export default function Nav() {
+  return (
+    <>
+      <nav>
+            <img src={logo} alt="logo"/>
+            <ul>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/reservations">Reservations</a></li>
+                <li><a href="/order-online">Order Online</a></li>
+            </ul>
+        <div>
+          <a href="/login">Login</a>
+          <a href="/signup">Sign up</a>
+        </div>
+      </nav>
+    </>
+  )
+}
