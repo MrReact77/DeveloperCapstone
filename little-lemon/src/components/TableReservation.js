a2a02bd823152a5acaeacfc0830393e2ea0de313
KarimDev7 <keldeyry@talentschmiede.com>
2023-08-17 13:55:51 +0200

    FEAT: TableReservation Grid Setup

--- little-lemon/src/components/TableReservation.js
+++ little-lemon/src/components/TableReservation.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 import "./TableReservation.css";
 
 export default function TableReservation() {
   return (
-      <div className="TableReservationGrid">
-          <div className="TableReservationContent">
-              TableReservation
-          </div>
+    <div className="TableReservationGrid">
+      <div className="TableReservationHeadingRow">
+        <div className="TableReservationHeadingContent">LittleLemon</div>
       </div>
-  )
+      <div className="TableReservationMainRow">
+        <div className="TableReservationMainContent">Table Reservation</div>
+      </div>
+    </div>
+  );
 }
